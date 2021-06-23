416281c4e4e20e8f60c44e9dc0751163202aff63
bearddan2000 <bearddan2000@yahoo.com>
2021-06-22 22:58:38 -0400

    initial commit

---
+++ bin/main.dart
@@ -0,0 +1,56 @@
+import 'package:dbcrypt/dbcrypt.dart';
+import 'package:cryptography/cryptography.dart';
+import 'package:convert/convert.dart';
+import 'dart:convert';
+
+Future<List<int>> longEncrypt(psw) async {
+
+  final pbkdf2 = Pbkdf2(
+    macAlgorithm: Hmac.sha256(),
+    iterations: 100000,
+    bits: 128,
+  );
+
+  // Password we want to hash
+  final secretKey = SecretKey(psw.codeUnits); //SecretKey([1,2,3]);
+
+  // A random salt
+  final nonce = [4,5,6];
+
+  // Calculate a hash that can be stored in the database
+  final newSecretKey = await pbkdf2.deriveKey(
+    secretKey: secretKey,
+    nonce: nonce,
+  );
+  final List<int> list = await newSecretKey.extractBytes();
+  return list;
+}
+
+Future<String> encrypt(psw) async {
+  List<int> encrypted = await longEncrypt(psw);
+  return new String.fromCharCodes(encrypted);
+}
+
+verify(psw1, psw2, hashed) async {
+  print("[VERIFY] $psw1\t$psw2");
+  psw1 = await encrypt(psw1);
+  var isCorrect = new DBCrypt().checkpw(psw1, hashed);
+  print("[VERIFY] $isCorrect");
+}
+
+Future<String> hash(psw) async {
+  print("[HASH] plainPassword: $psw");
+  psw = await encrypt(psw);  
+  var hashed = new DBCrypt().hashpw(psw, new DBCrypt().gensalt());
+  print("[HASH] hashedPassword: $hashed");
+  return hashed;
+}
+
+main() async {
+  String psw1 = "pass1234";
+  String psw2 = "1234pass";
+  String hash1 = await hash(psw1);
+  String hash2 = await hash(psw2);
+  verify(psw1, psw2, hash2);
+  verify(psw1, psw1, hash1);
+}
